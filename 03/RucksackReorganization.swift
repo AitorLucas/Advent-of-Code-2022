66bd586105727de0d245447ce9ab104821224308
Aitor Lucas <aitor.eler.lucas@gmail.com>
2022-12-15 18:10:57 -0300

    Add day 3 part two solution

--- 03/RucksackReorganization.swift
+++ 03/RucksackReorganization.swift
@@ -11,18 +11,9 @@ CrZsJsPPZsGzwwsLwLmpwMDw
 
 let bagsStrings = input.components(separatedBy: "\n")
 
-var sum = 0
-bagsStrings.forEach { bag in
-    let start = bag.startIndex
-    let middle = bag.index(start, offsetBy: bag.count/2)
-    let end = bag.endIndex
-    let firstHalf = Set(bag[start..<middle])
-    let lastHalf = Set(bag[middle..<end])
-
-    let diff = firstHalf.intersection(lastHalf)
+func calculatePriority(for diff: Set<Substring.Element>) -> Int {
     let character = Character(extendedGraphemeClusterLiteral: diff.first!)
     let asciiValue = Int(character.asciiValue!)
-
     var priority = 0
     // Lowercase
     if asciiValue >= 97 && asciiValue <= 122 {
@@ -35,6 +26,41 @@ bagsStrings.forEach { bag in
     else {
         fatalError("Character out of bounds: \(character)")
     }
+    return priority
+}
+
+var sum = 0
+bagsStrings.forEach { bag in
+    let start = bag.startIndex
+    let middle = bag.index(start, offsetBy: bag.count/2)
+    let end = bag.endIndex
+    let firstHalf = Set(bag[start..<middle])
+    let lastHalf = Set(bag[middle..<end])
+
+    let diff = firstHalf.intersection(lastHalf)
+   
+    let priority = calculatePriority(for: diff)
+    print(priority)
+    sum += priority
+}
+print("Sum:", sum)
+
+// - Part two
+let groupsBagsStrings = stride(from: 0, through: bagsStrings.count-1, by: 3).map({
+                            let start = bagsStrings.index(bagsStrings.startIndex, offsetBy: $0)
+                            let end = bagsStrings.index(start, offsetBy: 3, limitedBy: bagsStrings.endIndex) ?? bagsStrings.endIndex
+                            return Array(bagsStrings[start..<end])
+                        })
+
+sum = 0
+groupsBagsStrings.forEach { group in
+    let firstBag = Set(group[0])
+    let secondBag = Set(group[1])
+    let thirdBag = Set(group[2])
+    
+    let diff = firstBag.intersection(secondBag).intersection(thirdBag)
+    
+    let priority = calculatePriority(for: diff)
     print(priority)
     sum += priority
 }
